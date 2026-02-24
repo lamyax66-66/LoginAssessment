8a55d3a9bdb22a2c49926e9f150816a292ae99a2
lamyax66-66 <lamyax66@gmail.com>
2026-02-24 11:32:56 +0300

    fix: message

--- Login/src/app/enums/message.enum.ts
+++ Login/src/app/enums/message.enum.ts
@@ -1,5 +1,4 @@
 export enum MessageStyle {
- // ERROR ="rounded-full bg-error-bg text-error text-center border-2 border-error p-3 w-full shadow-error-sh",
-  MESSAGE ="rounded-full text-center border-2 p-3 w-full",
+  MESSAGE ="font-semibold rounded-full text-center border-2 p-3 w-full",
   ERROR = MESSAGE + " border-error text-error bg-error-bg shadow-error-sh",
 };
